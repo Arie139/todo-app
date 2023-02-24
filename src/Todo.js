78098440a23b7383945a4719edfbae6e1db8e9aa
Ariel <90644357+Arie139@users.noreply.github.com>
2023-02-24 17:56:53 -0500

    added styling

---
+++ src/Todo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {FaRegTrashAlt} from 'react-icons/fa'
+
+const style = {
+  li: `flex justify-between bg-slate-200 p-4 my-2 capitalize`,
+  row: `flex`,
+  text: `ml-2 cursor-pointer`
+}
+
+function Todo({todo}) {
+  return (
+    <li className={style.li}>
+      <div className={style.row}>
+        <input type="checkbox" />
+        <p className={style.text}>{todo}</p>
+      </div>
+      <button>{<FaRegTrashAlt />}</button>
+    </li>
+  )
+}
+
+export default Todo
